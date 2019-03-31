1412d2e42b070559b5d38cdc3685f6a7a8873dd5
Bob <crowcoder@gmail.com>
2019-03-31 09:41:10 -0400

    Added Path example

---
+++ PathToSuccess/Pages/Index.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.IO;
+
+namespace PathToSuccess.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string ThePath { get; set; }
+        public void OnGet()
+        {
+            ThePath = Path.Combine("one", "two", "three", "four");
+        }
+    }
+}
