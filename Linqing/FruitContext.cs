4170c2306de59617a7e7fdbd82b87a464eeaa4eb
Bob <crowcoder@gmail.com>
2019-03-31 16:10:18 -0400

    Began work on Linq project

---
+++ Linqing/FruitContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linqing
+{
+    public class FruitContext : DbContext
+    {
+        public FruitContext()
+            : base("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=Fruit; Integrated Security = True")
+        {
+
+        }
+        public DbSet<Fruit> Fruits { get; set; }
+    }
+
+    public class Fruit
+    {
+        [Key]
+        public int FruitID { get; set; }
+        public string FruitName { get; set; }
+        public bool? FruitIsYummy { get; set; }
+    }
+}
---
+++ Linqing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linqing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var fruitCtx = new FruitContext())
+                {
+                    var firstFruit = fruitCtx.Fruits.First();
+                    Console.WriteLine(firstFruit.FruitName);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
